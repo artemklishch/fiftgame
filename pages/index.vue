6315b638c6d7db13ddf5de0eff02119e6ca2c22c
artemklishch <artemklishch@gmail.com>
2020-12-24 13:19:46 +0200

    Merge bra

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <GameBlock />
+  </div>
+</template>
+
+<script>
+import GameBlock from "../components/GameBlock";
+
+export default {
+  components: {
+    GameBlock,
+  },
+};
+</script>
+
+<style lang='scss'>
+.container {
+  background-color: lightblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
